ddfd0cf702756f8fc385b658b6957972c0cc36fc
shivamrishi98 <shivamrishisr@gmail.com>
2021-02-22 09:41:39 +0530

    added recommendations response model

--- Spotify/Managers/ApiManager.swift
+++ Spotify/Managers/ApiManager.swift
@@ -104,11 +104,11 @@ final class ApiManager {
     
     public func getRecommendations(
         genres:Set<String>,
-        completion: @escaping ((Result<String,Error>) -> Void)
+        completion: @escaping ((Result<RecommendationsResponse,Error>) -> Void)
     ) {
         
         let seeds = genres.joined(separator: ",")
-        createRequest(with: URL(string: Constants.baseAPIURL + "/recommendations?seed_genres=\(seeds)"),
+        createRequest(with: URL(string: Constants.baseAPIURL + "/recommendations?limit=40&seed_genres=\(seeds)"),
                       type: .GET) { request in
             let task = URLSession.shared.dataTask(with: request) { (data, _, error) in
                 guard let data = data, error == nil else {
@@ -117,11 +117,9 @@ final class ApiManager {
                 }
 
                 do {
-                    let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
-//                    let result = try JSONDecoder().decode(Recommended.self, from: data)
-//
-//                    print(result)
-//                    completion(.success(result))
+
+                    let result = try JSONDecoder().decode(RecommendationsResponse.self, from: data)
+                    completion(.success(result))
 
                 }
 
---
+++ Spotify/Models/RecommendationsResponse.swift
@@ -0,0 +1,17 @@
+//
+//  RecommendationsResponse.swift
+//  Spotify
+//
+//  Created by Shivam Rishi on 22/02/21.
+//
+
+import Foundation
+
+struct RecommendationsResponse:Codable {
+    let tracks: [AudioTrack]
+}
+
+
+
+
+
