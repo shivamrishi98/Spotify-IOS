7ef14e62f195ce1278068a84576d67a8d7eee0c8
shivamrishi98 <shivamrishisr@gmail.com>
2021-02-24 11:38:21 +0530

    Created NewReleasesCollectionView Cell UI

---
+++ Spotify/ViewModels/NewReleasesCellViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  NewReleasesCellViewModel.swift
+//  Spotify
+//
+//  Created by Shivam Rishi on 24/02/21.
+//
+
+import Foundation
+
+struct NewReleasesCellViewModel {
+    let name:String
+    let artworkURL:URL?
+    let numberOfTracks:Int
+    let artistName:String
+}
