fdaa99caf4f8aa22627e1c9760831fc79233c937
shivamrishi98 <shivamrishisr@gmail.com>
2021-02-27 22:20:55 +0530

    setup playlist UI

---
+++ Spotify/ViewModels/PlaylistHeaderViewViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  PlaylistHeaderViewViewModel.swift
+//  Spotify
+//
+//  Created by Shivam Rishi on 27/02/21.
+//
+
+import Foundation
+
+
+struct PlaylistHeaderViewViewModel {
+    let name:String?
+    let owner:String?
+    let description:String?
+    let artworkURL:URL?
+}
