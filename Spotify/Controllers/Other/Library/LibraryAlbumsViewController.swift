1a602104cafaf4ea9aded94d20f20c4a3447d9f7
shivamrishi98 <shivamrishisr@gmail.com>
2021-03-08 10:40:00 +0530

    Setup Library UI

---
+++ Spotify/Controllers/Other/Library/LibraryAlbumsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryAlbumsViewController.swift
+//  Spotify
+//
+//  Created by Shivam Rishi on 08/03/21.
+//
+
+import UIKit
+
+class LibraryAlbumsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+}
---
+++ Spotify/Controllers/Other/Library/LibraryPlaylistsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryPlaylistsViewController.swift
+//  Spotify
+//
+//  Created by Shivam Rishi on 08/03/21.
+//
+
+import UIKit
+
+class LibraryPlaylistsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+
+    }
+    
+}
