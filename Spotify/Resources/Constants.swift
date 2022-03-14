11f4c382d9ce38be89cb625e916cec456a3eb0fe
shivamrishi98 <shivamrishisr@gmail.com>
2022-03-14 17:21:59 +0530

    Updated yaml

---
+++ Spotify/Resources/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Spotify
+//
+//  Created by Shivam Rishi on 14/03/22.
+//
+
+import Foundation
+
+let kCLIENTID = "8d34c9c9d16c447981b11948ace1412e"
+let kCLIENTSECRET = "69ee2b0a762e406fa1e846917560f2de"
+let kTOKENAPIURL = "https://accounts.spotify.com/api/token"
+let kREDIRECTURI = "Https://www.iosacademy.io"
+let kSCOPES = "user-read-private%20playlist-modify-public%20playlist-read-private%20playlist-modify-private%20user-follow-read%20user-library-modify%20user-library-read%20user-read-email"
